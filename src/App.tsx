18e6cabc08f99b77c62617702391501c87ea0e41
mwang840 <maxwang@udel.edu>
2023-05-08 13:00:49 -0400

    part one with form for university lookup!

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { EnterUniversity } from './components/EnterUniversity';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,17 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Rate My Course
+        </h1>
+        <EnterUniversity></EnterUniversity>
       </header>
     </div>
   );
--- src/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Navbar = () => {
-    return <nav className="navbar-container">
-        
-    </nav>
-}
